6193cdb1d07907c274b615374eb1453b748f91d0
GregoryFonseca <gregorycordeirofonseca@gmail.com>
2025-04-28 12:19:10 +0200

    Attempt number += 1

--- src/timol/__init__.py
+++ src/timol/__init__.py
@@ -1,12 +1,16 @@
 import pathlib
 
 package_dir = pathlib.Path(__file__).resolve().parent  # src/timol
-pycache_dir = package_dir / "__pycache__"
-
-num_cache_files = sum(1 for _ in pycache_dir.iterdir() if _.is_file())
-if num_cache_files == 1:
-    print(
-        "Timol is running for the first time: startup time will be slow due to bytecode compilation."
-    )
+flag_file = package_dir / ".initialized"
+if not flag_file.exists():
+    try:
+        flag_file.write_bytes(b"1")
+        print(
+            "Timol is running for the first time: startup time might be slower due to bytecode compilation and initialization"
+        )
+    except OSError:
+        # Not writeable or smth, so we just don't say anything
+        # It is what it is
+        pass
 
 from .cli import bmark, cli
